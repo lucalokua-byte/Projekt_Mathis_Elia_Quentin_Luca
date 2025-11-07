20df660d946fde22e1232b4f55535e6b894d88fa
Elia Amani <amanielia333@gmail.com>
2025-11-07 16:33:05 +0100

    Adds method to persist license plate data to file
    Introduces functionality to save updated license plate data to disk,
    ensuring changes are not lost between sessions and improving data reliability.

--- CRUDlicenaseplate/Db-maneger/Db_maneger.py
+++ CRUDlicenaseplate/Db-maneger/Db_maneger.py
@@ -20,8 +20,12 @@ class DBManger:
     def load_data(self):
         #Load data from the JSON file if it exists
         if os.path.exists(self.filename):
-            with open(self.filename,'r', encoding='utf-8') as file:
+            with open(self.filename,'r', encoding='utf-8') as file: # 'r' mode for reading
                 self.license_plates = json.load(file)
         else:
             self.license_plates = []
-            
+
+    def save_data(self):
+        #Save the current license plates to the JSON file
+        with open(self.filename, 'w', encoding='utf-8') as file: # 'w' mode for writing
+            json.dump(self.license_plates, file, indent=4) # indent=4 for pretty printing
