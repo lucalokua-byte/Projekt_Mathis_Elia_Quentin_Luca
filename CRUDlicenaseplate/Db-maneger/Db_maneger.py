938f87acb1ff28b3acbf0bf0b8411910595adba0
Elia Amani <amanielia333@gmail.com>
2025-11-10 17:49:28 +0100

    Adds methods to add and remove license plates
    Introduces functions to manage license plates in the database,
    allowing insertion only if not present and removal if found.
    Improves data integrity and simplifies license plate management.

--- CRUDlicenaseplate/Db-maneger/Db_maneger.py
+++ CRUDlicenaseplate/Db-maneger/Db_maneger.py
@@ -29,3 +29,26 @@ class DBManger:
         #Save the current license plates to the JSON file
         with open(self.filename, 'w', encoding='utf-8') as file: # 'w' mode for writing
             json.dump(self.license_plates, file, indent=4) # indent=4 for pretty printing
+
+    def add_license_plate(self, license_plate):
+
+        # Add a licrense plate to the database if it's not already present.
+        #Returns True if added, False if already exists.
+
+        if not self.find(license_plate):
+            self.license_plates.append({'license_plate': license_plate})
+            self.save_data()
+            return True
+        return False
+    
+    def remove_licence_plate(self, license_plate):
+
+        #Remove a license plate from the database if it exists.
+        #Returns True if removed, False if not found.
+
+        for record in self.license_plates:
+            if record['license_plate'] == license_plate:
+                self.license_plates.remove(record)
+                self.save_data()
+                return True
+        return False
